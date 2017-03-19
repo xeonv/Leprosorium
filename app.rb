ff422e27c83b742711718d488a4426a5e9af1058
Valery <xeonv@mail.ru>
2017-03-19 23:51:57 +0400

    Add textarea and button

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello!"			
 end
 
 get '/new' do
-  erb "Hello World"
+   erb :new
 end
